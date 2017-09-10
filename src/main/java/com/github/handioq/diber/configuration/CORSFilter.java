380bcd5bb6ec520700c0753b1de0cd6a96b13918
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-09-10 22:28:33 +0300

    Added Content-Type header to the CORS Filter.

--- src/main/java/com/github/handioq/diber/configuration/CORSFilter.java
+++ src/main/java/com/github/handioq/diber/configuration/CORSFilter.java
@@ -24,7 +24,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization, Content-Type");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);

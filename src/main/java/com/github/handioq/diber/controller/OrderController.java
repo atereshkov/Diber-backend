1dbc4b876551eeae8da34edf644bb2bfa81f8baf
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-02-24 23:01:46 +0300

    Change search logic for multi query

--- src/main/java/com/github/handioq/diber/controller/OrderController.java
+++ src/main/java/com/github/handioq/diber/controller/OrderController.java
@@ -74,15 +74,21 @@ public class OrderController {
             OrderSpecificationsBuilder builder = new OrderSpecificationsBuilder();
             Pattern patternSimple = Pattern.compile("(\\w+?)(:|<|>|!)([\\w ]+),");
             Pattern patternComplex = Pattern.compile("(\\w+?)[.]?(\\w+?)(:|<|>|!)([\\w ]+),");
-            if (search.contains(".")) {
-                Matcher matcher = patternComplex.matcher(search + ",");
-                while (matcher.find()) {
-                    builder.with(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(4));
-                }
-            } else {
-                Matcher matcher = patternSimple.matcher(search + ",");
-                while (matcher.find()) {
-                    builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+
+            String[] searchQueries = search.split(",");
+
+            for (String query : searchQueries) {
+                LOGGER.info("Search for {}", query);
+                if (query.contains(".")) {
+                    Matcher matcher = patternComplex.matcher(query + ",");
+                    //while (matcher.find()) {
+                        builder.with(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(4));
+                    //}
+                } else {
+                    Matcher matcher = patternSimple.matcher(query + ",");
+                    //while (matcher.find()) {
+                        builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+                    //}
                 }
             }
 
